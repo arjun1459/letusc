bee4c9df150efab3f24bc67f0f0f1ca821b737e9
arjun1459 <arjun1459@gmail.com>
2013-03-08 22:59:23 +0530

    to convert decimal to binary

---
+++ dec2bin.c
@@ -0,0 +1,17 @@
+/* to convert a decimal to binary number */
+#include<stdio.h>
+int main()
+{
+int num,k,i;
+printf("Enter the number in decimal form=");
+scanf("%d",&num);
+for(i=31;i>=0;i--)
+{
+k=num>>i;
+if(k&1==1)
+printf("1");
+else
+printf("0");
+}
+return 0;
+}
